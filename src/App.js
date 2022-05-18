271763f3f8bfc6a41676f63ccd0f5ebd1c3c6adb
Jérôme Desseaux <jerome.desseaux@gmail.com>
2022-05-18 08:38:14 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { Button } from '@mui/material';
 import './App.css';
+import Menu from './components/navigation/sidenav';
+import { logout } from './services/auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const handleLogout = () => {
+    logout();
+};
+
+const App = () => {
+    return (
+        <Menu title="Bibliochouette">
+            <p>Hello world!</p>
+            <Button onClick={() => handleLogout()}>Test</Button>
+        </Menu>
+    );
+};
 
 export default App;
